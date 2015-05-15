c1b3d38771619038113499226eebaf2b365c4488
mshessey <mshessey@gmail.com>
2015-05-15 15:01:48 +0000

    Fixed /0, Makes list of roots, Finds all roots

--- Opdracht4/Findroot.py
+++ Opdracht4/Findroot.py
@@ -5,5 +5,8 @@ import math
 f1 = lambda x: x*x -2
 bisection.findAllroots(f1,-2,2,0.01)
 print('-----------------------------------------------------------------------')
+f3 = lambda x: math.exp(x-2)-1
+bisection.findAllroots(f3,-2,4,0.01)
+print('-----------------------------------------------------------------------')
 f2 = lambda x:math.sin(math.pi*x)
-bisection.findAllroots(f2,1,9.5,0.01)
+bisection.findAllroots(f2,1,9,0.01)
--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -1,3 +1,34 @@
+
+def checklower(f,a):
+    while True:
+        try: 
+            f(a)
+            break            
+        except: 
+            #print('f(a) zero, widening lower side of interval - 0.111')
+            a = a-0.111
+    return a
+
+def checkupper(f,b):
+    while True:
+        try: 
+            f(b)
+            break            
+        except: 
+            #print('f(b) zero, widening upper side of interval +0.111')
+            b = b+0.111
+    return b
+
+def checkcenter(f,a,w):
+    while True:
+        try: 
+            f(w)
+            break            
+        except: 
+            w = 3*(w-a)/4
+            #print('f(w) zero, moved guess (w) left to: '+str(w))
+    return w
+
 def hasroot(f,a,b):
     if f(a) > 0:
         if f(b) < 0:
@@ -9,34 +40,46 @@ def hasroot(f,a,b):
         return False
 
 def combinterval(f,a,b,teeth):
-    print('Combing with '+str(teeth)+' teeth...')
+    #print('Combing with '+str(teeth)+' teeth...')
     dx = abs(b-a)/teeth
     if f(a) < 0:
         x = a
         for i in range(1,teeth):
             x += dx
-            if f(x) > 0:
-                return x
+            try:
+                f(x)
+                if f(x) > 0:
+                    return x
+            except:
+                continue
     elif f(a) <0:
         x = a
         for i in range(1,teeth):
             x += dx
-            if f(x) < 0:
-                return x
+            try:
+                f(x)
+                if f(x) < 0:
+                    return x
+            except:
+                continue
     else:
-        print("Combing didn't find candidates for root")
+        #print("Combing didn't find candidates for root")
         return False
     
 def findroot(f,a,b,epsilon):
     #print('Looking for root in ]'+str(a)+','+str(b)+'[')
     w = 0.5*(a+b)
+    a = checklower(f,a)
+    b = checkupper(f,b)
+    w = checkcenter(f,a,w)
+    
     if abs(a-b) < epsilon:
-        print('Foundroot (by squeeze) at: '+str(w))
+        #print('Foundroot (by squeeze) at: '+str(w))
         w = float(w)
         #print(type(w))
         return w
     if abs(f(w))<0.0001:
-        print('Foundroot (by direct) at: '+str(w))
+        #print('Foundroot (by direct) at: '+str(w))
         w = float(w)
         #print(type(w))
         return w
@@ -45,7 +88,7 @@ def findroot(f,a,b,epsilon):
     elif hasroot(f,w,b):
         w = findroot(f,w,b,epsilon)
     else:
-        print('Findroot failed to find root on interval.]'+str(a)+','+str(b)+'[.')
+        #print('Findroot failed to find root on interval.]'+str(a)+','+str(b)+'[.')
         teeth = 10
         
         while True:
@@ -56,7 +99,7 @@ def findroot(f,a,b,epsilon):
                 if gap < epsilon: # Dont do the next level of precision
                     return False
             else:
-                print('Combing worked, returning: '+str(myvar))
+                #print('Combing worked, returning: '+str(myvar))
                 return myvar
             
         print('Returning FALSE')
@@ -68,18 +111,14 @@ def findroot(f,a,b,epsilon):
 def findallrootinner(f,a,b,epsilon,outlist):
     root = findroot(f,a,b,epsilon)
     if root == False:
-        print("Finished Looking for roots")
+        #print("Finished Looking for roots")
         print(outlist)
         #print('The END!')
         return outlist
     else:
-        print('Passed root:'+ str(root))
+       # print('Passed root:'+ str(root))
         outlist.append(root)
-        #print(outlist)
-        #print('Root: '+str(root))
-        #print(type(root))
-        print('X variable temp value before root devision: '+str(x))
-        print('abs(x-root)'+str(abs(x-root)))
+       # print('About to divide out (x-root) factor')
         g = lambda x: f(x)/(x-root)
         findallrootinner(g,a,b,epsilon,outlist)
         return outlist
