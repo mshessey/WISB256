34292f9a9a88a70f97ebe76959160fbf082afa2a
mshessey <mshessey@gmail.com>
2015-05-16 14:04:33 +0000

    E: TicTacToe

--- Oefenopgaven/TicTacToe.py
+++ Oefenopgaven/TicTacToe.py
@@ -6,55 +6,65 @@ game = [line1,line2,line3]
 
 def horwin(line):
     original = line[0]
+    if original == '0':
+        return 'NO'
     for i in line:
         if i != original:
-            return 0 #no one
+            return 'NO' #no one
     return original
 
 
 def vertwin(col):
     original=line1[col]
+    if original == '0':
+        return 'NO'
     for line in game:
         if line[col] != original:
-            return 0
+            return 'NO'
     return original
     
 def downdiagwin():
     original = game[0][0]
+    if original == '0':
+        return 'NO'
     for i in range(1,3):
         if game[i][i] != original:
            # print('Tested: '+str(game[i][i])+' Against: '+str(original))
-            return 0
+            return 'NO'
     return original
     
 def updiagwin():
     original = game[2][0]
+    if original == '0':
+        return 'NO'
     for i in range(1,3):
         if game[2-i][i] != original:
-            return 0
+            return 'NO'
     return original
 
 
 
 def findwinner():
     ret = downdiagwin()
-    if ret != 0:
+    if ret != 'NO':
             #print('downdiagwin')
             return ret
     ret = updiagwin()
-    if ret != 0:
+    if ret != 'NO':
             #print('updiagwin')
             return ret
     for i in range (0,3):
         ret = horwin(game[i])
-        if ret != 0:
+        if ret != 'NO':
            # print('horwin in line: '+str(i))
             return ret
         ret = vertwin(i)
-        if ret != 0:
+        if ret != 'NO':
             #print('verwin in col: '+str(i))
             return ret
-    return 0   
+    return 'NO'   
+
+output = 'No winner'
 
 if findwinner()=='1':
     output = 'Player 1 Wins'
@@ -62,6 +72,4 @@ if findwinner()=='1':
 if findwinner()=='2':
     output = 'Player 2 Wins'
     
-if findwinner()==0:
-    output = 'No Winner'
 print(output)
