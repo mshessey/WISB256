8e9b098ebe078c791ed5e05ebc28eb01b1674f8a
mshessey <mshessey@gmail.com>
2015-05-19 08:52:21 +0000

    try again

--- Tentamen/RPN_rekenen.py
+++ Tentamen/RPN_rekenen.py
@@ -6,25 +6,16 @@ ops = {'+' : operator.add, '-' : operator.sub, '*' : operator.mul, '/': operator
 #answer = ops[words[2]](int(words[0]),int(words[1]))
 
 while True:
-    #print('Looking for words')
-    #print(words)
     alreadyfound = False
     if len(words) > 1:
-       # print('There are still words')
         for i in range(0,len(words)):
             if alreadyfound:
                 break
-          #  print('Trying word--: '+str(words[i]))
             for sign in ops:
-            #    print('Trying sign: '+str(sign))
                 if words[i] == sign:
-               #     print('Word matched sign')
                     words[i] = ops[words[i]](int(words[i-2]),int(words[i-1]))
-                 #   print(words)
                     words.pop(i-2)
-                   # print(words)
                     words.pop(i-2)
-                   # print(words)
                     alreadyfound = True
                     break
                 else:
